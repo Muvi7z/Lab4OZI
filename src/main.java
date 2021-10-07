b3494f931f02f87117c1c281c1c74d73254f65c5
Muvi7z <ochir-zhamsuev@mail.ru>
2021-10-07 20:26:39 +0800

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+    class Encryption{
+        String charRU = "АБВГДЕЁЖЗИЙКЛМНОПРСТЙФХЦЧШЩЪЫЬЭЮЯ";
+        String text ="";
+        String ciphertext ="";
+    }
+}
